881af816985677818d3e65c9d9324b377f692cb0
DnMakyr <dnsc1503@gmail.com>
2023-11-28 11:17:55 +0700

    Deleted unnecessary method in notification controller

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -10,30 +10,9 @@ class NotificationsController extends Controller
     public function index()
     {
         $notifications = auth()->user()->notifications;
-        $unreadNotificationsCount = auth()->user()->unreadNotifications->count();
+        auth()->user()->unreadNotifications->markAsRead();
         return response()->json([
             'notifications' => $notifications,
-            'unreadNotifications' => $unreadNotificationsCount
         ]);
-    }
-    public function markAsRead(Request $request)
-    {
-        $notification = auth()->user()->notifications()->where('id', $request->id)->first();
-        if ($notification) {
-            $notification->markAsRead();
-            return response()->json(['success' => 'Marked as read']);
-        } else {
-            return response()->json(['error' => 'Notification not found']);
-        }
-    }
-    public function markAllAsRead()
-    {
-        $notifications = auth()->user()->unreadNotifications;
-        if ($notifications) {
-            $notifications->markAsRead();
-            return response()->json(['success' => 'Marked all as read']);
-        } else {
-            return response()->json(['error' => 'Has read all']);
-        }
     }
 }
