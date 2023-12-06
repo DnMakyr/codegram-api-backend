c65e2e34a7adeb6bedb16ab613477db9748a7233
DnMakyr <dnsc1503@gmail.com>
2023-12-06 11:42:23 +0700

    Finished infinite scroll

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -19,7 +19,7 @@ class PostsController extends Controller
             ->with(['user' => function ($query) {
                 $query->with('profile');
             }, 'comments'])
-            ->paginate(3);
+            ->paginate(2);
 
         $suggests = User::whereNotIn('id', $users)
             ->where('id', '!=', auth()->user()->id)
